d7914e4fa9c5b0aa77b6b6b6a5f7ed71a6d8ad55
Hani Husamuddin <hani.husam@gmail.com>
2022-03-25 19:47:21 +0700

    refactor: change the link to fitfoodiefinds web

--- scraping.js
+++ scraping.js
@@ -4,85 +4,113 @@ const xlsx = require('xlsx');
 async function getLinks() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://www.forksoverknives.com/recipes/?type=grid');
+  await page.goto(
+    'https://fitfoodiefinds.com/recipe-search/?fwp_meal=lunch-dinner&fwp_diet=vegan'
+  );
   let links = [];
 
-  while ((await page.$('.pagination .next.disabled')) === null) {
+  while (await page.$('.pagination .pagination-next > a')) {
     const newLinks = await page.$$eval(
-      '.inner :not(.promo).post-item .container .row .col.content-block h3 a',
+      '.post .entry-header .entry-title > a',
       (allLinks) => allLinks.map((link) => link.href)
     );
 
     links = [...links, ...newLinks];
-    await page.click('.pagination .next a');
-    await page.waitForNavigation({ timeout: 0 });
+    try {
+      await page.waitForNetworkIdle({ timeout: 0 });
+      await page.click('.pagination .pagination-next > a');
+    } catch (error) {
+      break;
+    }
   }
+
+  const newLinks = await page.$$eval(
+    '.post .entry-header .entry-title > a',
+    (allLinks) => allLinks.map((link) => link.href)
+  );
+
+  links = [...links, ...newLinks];
 
   await browser.close();
   return links;
 }
 
-async function scrapeRecipe(id, link, page) {
-  await page.goto(link);
-  let name, category, ingredients, instructions;
+async function scrapeRecipe(link, page) {
+  try {
+    await page.goto(link);
+  } catch (error) {
+    console.log(error);
+  }
+
+  let name, ingredients, instructions, nutritions, review;
 
   try {
-    name = await page.$eval('.banner-block h1', (name) => name.innerText);
+    name = await page.$eval('.tasty-recipes-title', (name) => name.innerText);
   } catch (error) {
-    name = 'not a data';
+    name = 'No name';
   }
+
   try {
-    category = await page.$eval(
-      '.banner-block ul li:last-child a',
-      (category) => category.innerText
+    review = await page.$eval(
+      '.tasty-recipes-rating-label',
+      (name) => name.innerText
     );
   } catch (error) {
-    category = 'not a data';
+    review = 'No review';
   }
+
   try {
     ingredients = await page.$$eval(
-      '.section-ingredients li span',
+      '.tasty-recipes-ingredients ul > li',
       (ingredients) => ingredients.map((ingredient) => ingredient.innerText)
     );
   } catch (error) {
-    ingredients = 'not a data';
+    ingredients = 'No ingredients';
   }
+
   try {
     instructions = await page.$$eval(
-      '.section-instruction .instruction-box ol li',
+      '.tasty-recipes-instructions ol li',
       (instructions) => instructions.map((instruction) => instruction.innerText)
     );
   } catch (error) {
-    instructions = 'not a data';
+    instructions = 'No instructions';
+  }
+
+  try {
+    nutritions = await page.$$eval(
+      '.tasty-recipes-nutrition .nutrition-item',
+      (nutritions) => nutritions.map((instruction) => instruction.innerText)
+    );
+  } catch (error) {
+    nutritions = 'No nutritions';
   }
 
   return {
-    id,
     name,
-    category,
+    review,
     ingredients: ingredients.join('\n'),
     instructions: instructions.join('\n'),
+    nutritions: nutritions.join('\n'),
   };
 }
 
 async function main() {
-  let id = 1;
   const allLinks = await getLinks();
 
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  const scrapedData = [];
+  let scrapedData = [];
 
   for (let link of allLinks) {
-    const data = await scrapeRecipe(id, link, page);
-    scrapedData.push(data);
-    id++;
+    const data = await scrapeRecipe(link, page);
+    scrapedData = [...scrapedData, data];
   }
 
   const wb = xlsx.utils.book_new();
   const ws = xlsx.utils.json_to_sheet(scrapedData);
   xlsx.utils.book_append_sheet(wb, ws);
-  xlsx.writeFile(wb, 'recipes.xlsx');
+  xlsx.writeFile(wb, 'launch-recipes.xlsx');
 }
 
 main();
